163476f9b8778a472a411af986a15645b912daee
Dragon <dragon7780@hotmail.nl>
2015-01-02 19:29:02 +0100

    Make it a Fusion device
    Change-Id: I0392b71c6481af7905afb5ead41a7f607a6e4956
    Signed-off-by: Dragon <dragon7780@hotmail.nl>

---
+++ fusionsp.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, vendor/fusion/config/common_phone.mk)
+
+$(call inherit-product, vendor/fusion/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/samsung/klteusc/full_klteusc.mk)
+
+PRODUCT_DEVICE := klteusc
+PRODUCT_NAME := fusionsp_klteusc
